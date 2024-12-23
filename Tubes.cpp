cb5e26e9744f9dd8526a9d506f1c4fed4ee88952
Abilhzn <131866217+Abilhzn@users.noreply.github.com>
2024-12-23 18:33:13 +0700

    manage everything

--- Tubes.cpp
+++ Tubes.cpp
@@ -18,7 +18,7 @@ public:
 
 
 void generateItem(vector<Data>& items) {
-    // Menambahkan 300 item contoh ke dalam vector
+    // Menambahkan sekian item contoh ke dalam vector
     for (int i = 1; i <= 1000; ++i) {
         if (i % 3 == 1) {  // Kebutuhan Primer
             items.emplace_back("Barang Primer " + to_string(i), 1, 5000 + (i * 100));
@@ -31,12 +31,15 @@ void generateItem(vector<Data>& items) {
     // Mengacak urutan item
     srand(static_cast<unsigned int>(time(nullptr)));
     random_shuffle(items.begin(), items.end());
+    
+    for (const auto& item : items) {
+        cout << "Nama Barang: " << item.nama_barang << ", Kode Barang: " << item.kode_barang << ", Harga Barang: " << item.harga_barang << endl;
+    }
 }
 
-void iterative_shopping_list(vector<Data>& items) {
+void insertionSort(vector<Data>& items) {
     int n = items.size();
     for (int i = 1; i < n; ++i) {
-        // int key = items[i].kode_barang;
         Data temp_item = items[i];
         int j = i - 1;
         while (j >= 0 && items[j].kode_barang > temp_item.kode_barang) {
@@ -47,63 +50,49 @@ void iterative_shopping_list(vector<Data>& items) {
     }
 }
 
-vector<Data> merge(const vector<Data>& left, const vector<Data>& right) {
+vector<Data> mergeSort(const vector<Data>& left, const vector<Data>& right) {
     vector<Data> result;
     int i = 0, j = 0;
-
-    // Kombinasikan dua bagian yang sudah diurutkan secara rekursif
     if (left.empty()) {
         return right;
-    } 
+    }
     if (right.empty()) {
         return left;
     }
-
     if (left[i].kode_barang <= right[j].kode_barang) {
         result.push_back(left[i]);
-        vector<Data> merged_rest = merge(vector<Data>(left.begin() + 1, left.end()), right);
+        vector<Data> merged_rest = mergeSort(vector<Data>(left.begin() + 1, left.end()), right);
         result.insert(result.end(), merged_rest.begin(), merged_rest.end());
     } else {
         result.push_back(right[j]);
-        vector<Data> merged_rest = merge(left, vector<Data>(right.begin() + 1, right.end()));
+        vector<Data> merged_rest = mergeSort(left, vector<Data>(right.begin() + 1, right.end()));
         result.insert(result.end(), merged_rest.begin(), merged_rest.end());
     }
-
     return result;
 }
 
-// Fungsi rekursif untuk Merge Sort
 vector<Data> merge_sort(const vector<Data>& items) {
     if (items.size() <= 1) {
         return items;
     }
-
-    // Pecah array menjadi dua bagian
     int mid = items.size() / 2;
     vector<Data> left(items.begin(), items.begin() + mid);
     vector<Data> right(items.begin() + mid, items.end());
-
-    // Rekursif untuk mengurutkan bagian kiri dan kanan
     left = merge_sort(left);
     right = merge_sort(right);
-
-    // Gabungkan kedua bagian yang sudah diurutkan
-    return merge(left, right);
+    return mergeSort(left, right);
 }
 
 int main() {
     vector<Data> items;
-    generateItem(items);
-    // iterative_shopping_list(items);
+
+    insertionSort(items);
     // items = merge_sort(items);
 
-    for (const auto& item : items) {
-        cout << "{'" << item.nama_barang << "', " << item.kode_barang << ", " << item.harga_barang << "}," << endl;
-    }
+//    for (int i = 0; i < items.size(); i++){
+//        cout << "Nama Barang: " << items[i].nama_barang << ", Kode Barang: " << items[i].kode_barang << ", Harga Barang: " << items[i].harga_barang << endl;
+//    }
 
-    // for (int i = 0; i < items.size(); i++){
-    //     cout << "Nama Barang: " << items[i].nama_barang << ", Kode Barang: " << items[i].kode_barang << ", Harga Barang: " << items[i].harga_barang << endl;
-    // }
+    cout << "data is sorteed!" << endl;
     return 0;
 }
-
